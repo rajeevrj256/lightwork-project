a14bd44206ada3d3778ae31bc614c99e6c42532f
rajeevrj256 <rjrajeev5918@gmail.com>
2024-03-23 23:40:02 +0530

    up

--- backend/csv_to_api.py
+++ backend/csv_to_api.py
@@ -10,7 +10,7 @@ CORS(app)
 def get_excel_data():
     # Read CSV data using pandas
     excel_data = pd.read_csv("NIFTY 50_Historical_PR_01032023to21032024.csv")
-    
+    #covert date into timestamp
     excel_data['Date']=pd.to_datetime(excel_data['Date'])
     # Sort DataFrame based on the 'Date' column
     excel_data_sorted = excel_data.sort_values(by='Date')
