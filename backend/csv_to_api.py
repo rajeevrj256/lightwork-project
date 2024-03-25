6139265f1fa9eafdf9fb79598c294e59c3dad9af
rajeevrj256 <rjrajeev5918@gmail.com>
2024-03-25 17:29:50 +0530

    excel data into csv data

--- backend/csv_to_api.py
+++ backend/csv_to_api.py
@@ -10,20 +10,20 @@ CORS(app)
 def get_excel_data():
     
     # Read CSV data using pandas
-    excel_data = pd.read_csv("NIFTY 50_Historical_PR_01032023to21032024.csv")
+    csv_data = pd.read_csv("NIFTY 50_Historical_PR_01032023to21032024.csv")
    
     
     # Drop the "Index Name" column
-    excel_data.drop(columns=["Index Name"], inplace=True)
+    csv_data.drop(columns=["Index Name"], inplace=True)
     
 
      #covert date into timestamp
-    excel_data['Date']=pd.to_datetime(excel_data['Date'],dayfirst=True)
+    csv_data['Date']=pd.to_datetime(csv_data['Date'],dayfirst=True)
     # Sort DataFrame based on the 'Date' column
-    excel_data_sorted = excel_data.sort_values(by='Date')
+    csv_data_sorted = csv_data.sort_values(by='Date')
     
     # Convert DataFrame to JSON
-    json_data = excel_data_sorted.to_json(orient='records')
+    json_data = csv_data_sorted.to_json(orient='records')
     #not use return jsonify(json_data) because data should be in array formate.Because megerdfunction in frontend only deal with data of arrays.
     return json_data
 
